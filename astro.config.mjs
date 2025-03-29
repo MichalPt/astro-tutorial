a82c4016ebef417fac71a43c349067b2a64b5507
Michal Ptacek <michalptacek@outlook.com>
2025-03-29 20:16:16 +0100

    Interactive buttons added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-  site: "https://my-astrotutorial.netlify.app"
+  site: "https://my-astrotutorial.netlify.app",
+  integrations: [preact()]
 });
